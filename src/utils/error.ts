36c7f96e7a6b78c6ff1257295baf4990131914e1
Wutipat Khamnuansin <bellmcp@gmail.com>
2024-06-23 19:43:33 +0700

    Handle 401 redirect to login page

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,32 +1,63 @@
 import { get, isEmpty } from 'lodash'
 import * as uiActions from 'modules/ui/actions'
+import { eraseCookie } from 'utils/cookies'
+
+const PATH = process.env.REACT_APP_BASE_PATH
 
 export const handleApiError = (
   err: any,
   dispatch: any,
-  overrideMessage?: string
+  templateMessage: string = ''
 ) => {
-  if (!isEmpty(get(err, 'response.data.mesg', ''))) {
-    dispatch(uiActions.setFlashMessage(get(err, 'response.data.mesg'), 'error'))
-  } else if (overrideMessage !== '') {
-    const status = get(err, 'response.status', 'บางอย่าง')
+  const status = get(err, 'response.status', '')
+  const errorMessagefromAPI = get(err, 'response.data.mesg', '')
+
+  if (!isEmpty(errorMessagefromAPI)) {
     dispatch(
       uiActions.setFlashMessage(
-        `${overrideMessage} เกิดข้อผิดพลาด ${status}`,
+        `<b>${
+          templateMessage ? `${templateMessage} ` : ''
+        }เกิดข้อผิดพลาด ${status}</b><br/>${errorMessagefromAPI}`,
         'error'
       )
     )
   } else {
     const status = get(err, 'response.status', '')
-    const title = get(err, 'response.data.title', '')
+    const title = get(err, 'response.statusText', '')
     const message = get(err, 'message', 'โปรดลองใหม่อีกครั้ง')
     dispatch(
       uiActions.setFlashMessage(
-        `<b>เกิดข้อผิดพลาด ${status}</b><br/>${
-          !isEmpty(title) ? title : message
-        }`,
+        `<b>${
+          templateMessage ? `${templateMessage} ` : ''
+        }เกิดข้อผิดพลาด ${status}</b><br/>${!isEmpty(title) ? title : message}`,
         'error'
       )
     )
+  }
+
+  // session expired
+  if (status === 401) {
+    eraseCookie('token')
+    eraseCookie('firstName')
+    eraseCookie('lastName')
+    eraseCookie('id')
+    eraseCookie('workplace')
+    eraseCookie('contact')
+    eraseCookie('seal') // for ocsc job
+    eraseCookie('ministry') // for ocsc job
+    eraseCookie('ministryId') // for ocsc job
+    eraseCookie('department') // for ocsc job
+    eraseCookie('departmentId') // for ocsc job
+
+    const from = sessionStorage.getItem('from')
+    if (from !== '' && from !== null && from !== undefined) {
+      sessionStorage.removeItem('from')
+      window.location.href = from
+    } else {
+      setTimeout(() => {
+        window.location.href = `${PATH}/login`
+        window.location.reload()
+      }, 2000)
+    }
   }
 }
